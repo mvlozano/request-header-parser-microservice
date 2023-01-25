85e105b63f1c7e18d168957eeae2cd1a2e0c1dab
Marcos <marcosrvlzero69@gmail.com>
2023-01-24 23:20:19 -0500

    Comments Added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 //Middlewares
 //Enable requests from any domain
